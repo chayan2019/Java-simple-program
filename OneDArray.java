c620f71a519800804c451b8c7e9ca11762198265
chayanchowdhury <chayanchowdhury@gmail.com>
2016-12-28 08:27:31 +0530

    Insert Specific Element In last Position Array

---
+++ OneDArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class OneDArray {
+
+    public static void main(String[] args) {
+      
+      int [] listOfElement={5,10,15,20,25,30,35,40,45,50,60,70,75,80,85,90,95,100};  
+      
+      for (int i = 0; i < listOfElement.length; i++) {
+        System.out.println("List of element in Array : " +listOfElement[i]);
+      }
+         
+    }
+    
+  }
