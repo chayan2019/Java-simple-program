7030ed1c4c6ca6ffd3d326377daef15e601d5430
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-04 15:33:19 +0530

    practice1

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        // How to do Sum of Two Numbers
+        // initialize variable
+        int fNum=10;
+        int lNum=20;
+        int total=0;
+        total=fNum+lNum;
+        System.out.println("Sum of Two Numbers"+"  : " +total);
+    }
+
+}
