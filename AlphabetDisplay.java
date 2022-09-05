998286ed675eaf5515a94f817e30d1d373e145ad
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-05 20:28:57 +0530

    how to display alphabet

---
+++ AlphabetDisplay.java
@@ -0,0 +1,10 @@
+public class AlphabetDisplay {
+
+    public static void main(String[] args) {
+        System.out.println("how to display alphabet");
+        char i;
+        for (i = 'a'; i <= 'z'; i++) {
+            System.out.println("Display alphabet : " +i);
+        }
+    }
+}
