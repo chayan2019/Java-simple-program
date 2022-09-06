7cdea4810287f5865f6860269b90dcf1967b6c2b
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-06 07:28:20 +0530

    Practice

---
+++ CheckOddOrEven.java
@@ -0,0 +1,20 @@
+public class CheckOddOrEven {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // How to Check Given character is vowel or constant .
+
+        char checkCharacter= 'w';
+        if(checkCharacter == 'a' || checkCharacter == 'e' || checkCharacter == 'i' || checkCharacter == 'o' || checkCharacter == 'u' ){
+            System.out.println(checkCharacter + " is vowel");
+
+        }else{
+            System.out.println(checkCharacter + " is consonant");
+
+        }
+
+    }
+    }
