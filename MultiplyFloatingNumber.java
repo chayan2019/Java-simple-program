843f2c362081c4f58878d3117a7f72d764e7a5dc
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-06 14:03:30 +0530

    How to get Floating multiply value

--- MultiplyFloatingNumber.java
+++ MultiplyFloatingNumber.java
@@ -1,8 +1,8 @@
 public class MultiplyFloatingNumber {
 
     public static void main(String[] args) {
-        float a=2.2f;
-        float b=3.4f;
+        float a=2.22f;
+        float b=3.43f;
         float multiplyValue=a*b;
         System.out.println("Multiply Floating two numbers : " +multiplyValue);
     }
