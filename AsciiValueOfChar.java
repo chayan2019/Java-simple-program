72ac7edc092233014e46eabc0fcf4479f707134e
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-06 14:02:45 +0530

    How to get ASCII value of given Character

--- AsciiValueOfChar.java
+++ AsciiValueOfChar.java
@@ -1,7 +1,7 @@
 public class AsciiValueOfChar {
 
     public static void main(String[] args) {
-        char ch = 'c';
+        char ch = 'a';
         // converting char to int
         int asciivalue=(int)ch;
         System.out.println("The ASCII value of " + ch + " is: " + asciivalue);
