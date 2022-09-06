27d4843621155d117b0e8e0aff67e2ac4c271df9
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-06 14:07:55 +0530

    How to Swap Two Numbers

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -4,11 +4,11 @@ public class SwapTwoNumbers {
         
         int fNum=10,sNum=15;
         int swapNum=0;
-        System.out.println("Before swap value for fNum : " +fNum +" and "+sNum);
+        System.out.println("Before swap value for fNum:" +fNum +" and "+sNum);
         swapNum=fNum;
         fNum=sNum;
         sNum=swapNum;
-        System.out.println("After swap value for fNum : " +fNum +" and "+sNum);
+        System.out.println("After swap value for fNum:" +fNum +" and "+sNum);
 
     }
 
