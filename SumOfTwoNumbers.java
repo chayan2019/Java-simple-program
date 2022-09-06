44c8f236bf047da30e6af7e0a1ca330e7847535f
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-06 13:47:27 +0530

    How to do Sum of Two Numbers

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -2,12 +2,12 @@ public class SumOfTwoNumbers {
 
     public static void main(String[] args) {
         // How to do Sum of Two Numbers
-        // initialize variable
-        int fNum=10;
-        int lNum=20;
+        // initialize variable 
+        int fNum=30;
+        int lNum=40;
         int total=0;
         total=fNum+lNum;
-        System.out.println("Sum of Two Numbers"+"  : " +total);
+        System.out.println("Sum of Two Number"+"  : " +total);
     }
 
 }
