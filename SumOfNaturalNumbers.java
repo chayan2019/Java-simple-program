34857d9472023447c5aea79a2f90978273f19be9
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-07 18:18:24 +0530

    How to calculate sum of natural numbers

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+public class SumOfNaturalNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("How to calculate sum of natural numbers");
+        int sumTotal=0;
+        for(int i=0;i<100;i++){
+            sumTotal=sumTotal+i;
+        } 
+        System.out.println("Total sum"+sumTotal);
+    }
+}
