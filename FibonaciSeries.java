cd6ddc2d9b92628ab7b1222e7ee5f594e4c6b5b9
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-08 11:57:32 +0530

    How to print Fibinaci Series

---
+++ FibonaciSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class  FibonaciSeries {
+    
+    public static void main(String[] args) {
+
+    System.out.println("How to print Fibinaci Series");
+    Scanner scanner=new Scanner(System.in);
+    int n = scanner.nextInt();
+    //Fibinaci Series ::--- 0,1,1,2,3,5,8,13,21,34...
+    int firstNum=0;
+    int secNumber=1;
+    int next=0;
+    
+    for (int i = 0; i < n; i++) {
+        System.out.print(firstNum + ", ");
+        next=firstNum+secNumber;
+        firstNum=secNumber;
+        secNumber=next;
+    } 
+
+    }
+}
