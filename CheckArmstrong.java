23b9922db4f0800e32f887c94bff0a0cfb428316
chayan chowdhury <chayanchowdhury@gmail.com>
2022-09-10 06:53:04 +0530

    How to check given number is Armstrong or not

--- CheckArmstrong.java
+++ CheckArmstrong.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class CheckArmstrong {
 
     public static void main(String[] args) {
         System.out.println("How to check given number is Armstrong or not");
+        Scanner _Scanner =new Scanner(System.in);
+        int number = _Scanner.nextInt();
+        int remainder,originalNumber,result=0;
+        originalNumber = number;
+        while(originalNumber>0){
+              remainder=originalNumber%10;
+              result += Math.pow(remainder, 3);
+              originalNumber=originalNumber/10;
+        }
+        if(number == result){
+            System.out.println("Armstrong Numbers");
+        }else{
+            System.out.println("Not an Armstrong Number");
+
+        }
     }
 }
