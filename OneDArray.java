4ee9749e07b60e167aed5aef8372ae8777bf141d
chayanchowdhury <chayanchowdhury@gmail.com>
2022-09-22 08:53:17 +0530

    one D Array

--- OneDArray.java
+++ OneDArray.java
@@ -4,12 +4,23 @@ public class OneDArray {
 
     public static void main(String[] args) {
       
+      
       int [] listOfElement={5,10,15,20,25,30,35,40,45,50,60,70,75,80,85,90,95,100};  
+      
+      int [] newArray=new int[5];
       
       for (int i = 0; i < listOfElement.length; i++) {
         System.out.println("List of element in Array : " +listOfElement[i]);
       }
-         
+       
+      newArray[0]=1000;
+      newArray[1]=1001;
+      newArray[2]=1002;
+      newArray[3]=1003;
+      newArray[4]=1004;
+
+      System.out.println("One D Array :     " +Arrays.toString(newArray));
+
     }
     
   }
