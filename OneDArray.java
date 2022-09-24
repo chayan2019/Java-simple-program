9529a81ad42bbaed0316116f710f02a9f1813f19
chayanchowdhury <chayanchowdhury@gmail.com>
2022-09-24 13:17:08 +0530

    One D Array implemtation.

--- OneDArray.java
+++ OneDArray.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 public class OneDArray {
 
     public static void main(String[] args) {
-      
-      
+            
       int [] listOfElement={5,10,15,20,25,30,35,40,45,50,60,70,75,80,85,90,95,100};  
       
       int [] newArray=new int[5];
