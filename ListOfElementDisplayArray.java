cf5cc5de1491e28e0d000af7716169afcfadfe48
chayanchowdhury <chayanchowdhury@gmail.com>
2022-09-26 08:38:24 +0530

    Display List of Arraygit status

---
+++ ListOfElementDisplayArray.java
@@ -0,0 +1,17 @@
+public class ListOfElementDisplayArray {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Display List of Array!!");
+
+    int [] listOfElement={10,23,34,45,7,8,8,23,56,7,56,78,43,435,67,78,9,64,45,34,32,23,23,2,23,3};  
+
+    for(int i=0;i<listOfElement.length;i++){
+
+        System.out.println("Elements In Array" +listOfElement[i]);
+
+    }
+
+  }
+  
+}
