8657cfcc14bb5075392894326b6a92f12cfbbb0e
chayanchowdhury <chayanchowdhury@gmail.com>
2022-10-04 19:12:46 +0530

    SubtractionTwoDArray

--- SubtractionTwoDArray.java
+++ SubtractionTwoDArray.java
@@ -10,11 +10,16 @@ int b[][]={{1,3,4},{2,4,3},{1,2,4}};
     
 int c[][]=new int[3][3]; 
     
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-c[i][j]=a[i][j]-b[i][j];   
-System.out.print(c[i][j]+" ");    
-}    
+
+   for(int i=0;i<3;i++){    
+    
+    for(int j=0;j<3;j++){    
+     
+        c[i][j]=a[i][j]-b[i][j];   
+        
+
+        System.out.print(c[i][j]+" ");    
+   }    
 System.out.println();//new line    
     
 }
